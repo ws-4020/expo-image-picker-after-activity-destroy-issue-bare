b1c320f29071791e546bd401cdc31e57fd132c8b
creature-water-valley <83949308+creature-water-valley@users.noreply.github.com>
2022-09-12 11:39:38 +0900

    add patch file

--- ImagePickerExample.jsx
+++ ImagePickerExample.jsx
@@ -1,12 +1,13 @@
-import React, {useState} from 'react';
+import React, {useCallback, useState} from 'react';
 import {Button, Image, View} from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 
 export default function ImagePickerExample() {
-  const [image, setImage] = useState(null);
+  const [cameraImage, setCameraImage] = useState(null);
+  const [libraryImage, setLibraryImage] = useState(null);
 
-  const pickImage = async () => {
-    // No permissions request is necessary for launching the image library
+  const launchCamera = useCallback(async () => {
+    // No permissions request is necessary for launching the cameraImage library
     let result = await ImagePicker.launchCameraAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
@@ -14,17 +15,32 @@ export default function ImagePickerExample() {
       quality: 1,
     });
 
-    console.log(result);
+    if (!result.cancelled) {
+      setCameraImage(result.uri);
+    }
+  }, []);
+
+  const launchImageLibrary = useCallback(async () => {
+    // No permissions request is necessary for launching the cameraImage library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
 
     if (!result.cancelled) {
-      setImage(result.uri);
+      setLibraryImage(result.uri);
     }
-  };
+  }, []);
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button title="Pick an image from camera roll" onPress={pickImage} />
-      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      <Button title="Pick an image from camera roll" onPress={launchCamera} />
+      {cameraImage && <Image source={{ uri: cameraImage }} style={{ width: 100, height: 100 }} />}
+      <View style={{height: 10}}/>
+      <Button title="Pick an image from camera roll" onPress={launchImageLibrary} />
+      {libraryImage && <Image source={{ uri: libraryImage }} style={{ width: 100, height: 100 }} />}
     </View>
   );
 }
